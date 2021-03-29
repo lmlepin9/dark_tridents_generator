b205badb387eb2b8cc8926b14477623910287413
Anyssa Navrer-Agasson <anavrera@fnal.gov>
2021-03-29 04:34:44 -0500

    Add environment setup script

---
+++ setup_dm_grid.sh
@@ -0,0 +1,11 @@
+source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups.sh
+setup jobsub_client
+setup ifdhc
+
+echo -e "\033[1;31m Now re-setting some environmental variables \033[0m"
+EXPERIMENT=uboone
+JOBSUB_GROUP=uboone
+SAM_STATION=uboone
+GROUP=uboone
+SAM_EXPERIMENT=uboone
+SAM_GROUP=uboone
